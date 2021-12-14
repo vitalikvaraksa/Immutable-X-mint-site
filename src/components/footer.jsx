90766c7ad8305f9e98eaa2d2265fe7f2adac9fe7
vitalikvaraksa <Al.satoshi9997@gmail.com>
2021-12-14 04:43:20 +0300

    change image to video in footer section

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,16 +3,24 @@ export const Footer = (props) => {
     <div id='footer' className='text-center'>
       <div className="row main-member">
         <div className="col-sm-12 col-md-3">
-          <img src="img/tier1.png" className="tier" />
+          <video autoPlay muted style={{width:'100%'}}>
+            <source src="video/COMMON.mp4" type="video/mp4"/>
+          </video>
         </div>
         <div className="col-sm-12 col-md-3">
-          <img src="img/tier2.png" className="tier" />
+          <video autoPlay muted style={{width:'100%'}}>
+            <source src="video/RARE.mp4" type="video/mp4"/>
+          </video>
         </div>
         <div className="col-sm-12 col-md-3">
-          <img src="img/tier3.png" className="tier" />
+          <video autoPlay muted style={{width:'100%'}}>
+            <source src="video/EPIC.mp4" type="video/mp4"/>
+          </video>
         </div>
         <div className="col-sm-12 col-md-3">
-          <img src="img/tier4.png" className="tier" />
+          <video autoPlay muted style={{width:'100%'}}>
+            <source src="video/LEGENDARY.mp4" type="video/mp4"/>
+          </video>
         </div>
       </div>
     </div>
